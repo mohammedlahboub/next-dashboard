2926a77d4dd0405460b0fb9df7e32f48d16072d5
Mohammed Lahboub <104053591+mohammedlahboub@users.noreply.github.com>
2024-06-18 20:14:35 -0700

    3

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,8 +1,53 @@
+import Pagination from "@/app/ui/dashboard/pagination/pagination"
+import styles from "@/app/ui/dashboard/products/products.module.css"
+import Search from "@/app/ui/dashboard/search/search"
+import Image from "next/image"
+import Link from "next/link"
 
 const ProductPage = ()=> {
   return (
-    <div>
-ProductPage
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a product"/>
+        <Link href="/dashboard/product/add">
+        <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+          <thead>
+            <tr>
+              <td>Title</td>
+              <td>Description</td>
+              <td>Price</td>
+              <td>Created At</td>
+              <td>Stock</td>
+              <td>Action</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <div className={styles.user}>
+                  <Image src="/noavatar.png" alt="" width={40} height={40} className={styles.userImage} />
+              IPhone
+                </div>
+              </td>
+              <td>Lorem ipsum dolor sit amet consectetur.</td>
+              <td>$1560</td>
+              <td>10.12.2023</td>
+              <td>58</td>
+              <td>
+                <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.view}`}>View</button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+                </div>
+              </td>
+              </tr>
+          </tbody>
+      </table>
+      <Pagination/>
     </div>
   )
 }
